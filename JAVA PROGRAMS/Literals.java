3a4d9212c837e68951230e3ecee4d0ee858dcd44
Sameer Upadhyay <itssamyhere17@gmail.com>
2022-08-21 23:39:45 +0530

    s

---
+++ JAVA PROGRAMS/Literals.java
@@ -0,0 +1,30 @@
+public class Literals {
+    public static void main(String[] args) {
+
+        byte num1=80;      //80 will be known as a byte literal
+        short num2=72;     //72 will be known as a short literal
+        int num3=1704;     //1704 will be known an int literal
+        long num4=218386l; //218386 will be known as a long literal
+
+        float num5=10.1f;  //10.1 will be known as a float literal
+        double num6=10.32; //10.32 will be known as a double literal
+
+        char s='U';        //"U" will be known as a char literal
+
+        boolean k=false;    //true will be known as a boolen literal
+
+        String str="Sameer";//Sameer will be known as a string literal
+
+        
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(s);
+        System.out.println(k);
+        System.out.println(str);
+
+    }
+}
