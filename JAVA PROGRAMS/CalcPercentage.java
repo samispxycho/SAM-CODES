0b86c45e4c2c18a9cf5a120222d84c6ce9d86fd2
Sameer Upadhyay <itssamyhere17@gmail.com>
2022-08-23 18:13:38 +0530

    s

--- JAVA PROGRAMS/CalcPercentage.java
+++ JAVA PROGRAMS/CalcPercentage.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ProgramQues1 {
+public class CalcPercentage {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
