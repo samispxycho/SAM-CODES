8cde8ff362be5a54a7f8b9966c63548daaf9df9f
Sameer Upadhyay <itssamyhere17@gmail.com>
2022-08-24 15:58:33 +0530

    a

---
+++ JAVA PROGRAMS/Operators.java
@@ -0,0 +1,65 @@
+public class Operators {
+    public static void main(String[] args) {
+        //int num1=10;
+        //int num2=5;
+
+        //Assignment operators
+            //int result=num1+num2;
+            //System.out.println("Addition of the numbers is "+result);
+
+            //int result=num1-num2;
+            //System.out.println("Subtraction of the numbers is "+result);
+
+            //int result=num1*num2;
+            //System.out.println("Multiplication of the numbers is "+result);
+
+            //int result=num1/num2;
+            //System.out.println("Division of the numbers is "+result);
+
+            //System.out.println("Post imcrementing "+num1++);
+            //System.out.println(num1);     //Post incrementation
+
+            //System.out.println("Pre incrementing "+ ++num1); //Pre incrementation
+
+            //System.out.println("Post decrementing "+ num2--);
+            //System.out.println(num2);       //Post decrementation
+
+            //System.out.println("Pre incrementing "+ --num2);   //Pre decrementation
+
+        //Assignment operators
+            //int num3=10;
+            //System.out.println(num3);
+
+            //num1 +=3;
+            //System.out.println("Default value + 3 is "+ num1);
+
+            //num2 -=3;
+            //System.out.println("Default value -3 is "+num2);
+
+            //num1 *=3;
+            //System.out.println("Default value *3 is "+ num1);
+
+            //num2 /= 3;
+            //System.out.println("Default value /3 is "+num2);
+
+        //Comparision operators
+            //System.out.println(num1==num2);
+
+            //System.out.println(num1>num2);
+
+            //System.out.println(num1<num2);
+
+            //System.out.println(num1>=num2);
+
+            //System.out.println(num1<=num2);
+
+        //Logical operators [For value to be true]
+            //System.out.println(num1>2 && num2>3);    //both should be true
+
+            //System.out.println(num1>2||num1<3);      //one should be true
+
+            //System.out.println(num1!=num2);          //num1 is not equals to num2
+
+        //Bitwise operator not needed
+    }
+}
