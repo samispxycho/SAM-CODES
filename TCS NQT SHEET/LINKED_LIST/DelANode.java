6b8ee71c9661a27c5e9dd3343385ac03a2548714
Sameer Upadhyay <itssamyhere17@gmail.com>
2023-11-11 17:45:51 +0530

    code

---
+++ TCS NQT SHEET/LINKED_LIST/DelANode.java
@@ -0,0 +1,17 @@
+package LINKED_LIST;
+
+public class DelANode {
+
+    //delete a node without head
+
+    /*
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+    */
+
+    public static void main(String[] args) {
+        
+    }
+}
