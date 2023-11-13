8c6095868a4ec67628429a9f1ecc0a39aafb2caa
Sameer Upadhyay <itssamyhere17@gmail.com>
2023-11-13 20:10:51 +0530

    codes

---
+++ TCS NQT SHEET/LINKED_LIST/DetectCycle.java
@@ -0,0 +1,25 @@
+package LINKED_LIST;
+
+public class DetectCycle {
+
+    /*
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null)return false;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast.next!=null && fast.next.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow)return true;
+        }
+        return false;
+    }
+    */
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ TCS NQT SHEET/LINKED_LIST/FindIntersection.java
@@ -0,0 +1,32 @@
+package LINKED_LIST;
+
+public class FindIntersection {
+
+    /*
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null)return null;
+
+        ListNode dummy1 = headA;
+        ListNode dummy2 = headB;
+
+        while(dummy1 != dummy2){
+            if(dummy1 == null){
+                dummy1 = headB;
+            }else{
+                dummy1 = dummy1.next;
+            }
+
+            if(dummy2 == null){
+                dummy2 = headA;
+            }else{
+                dummy2 = dummy2.next;
+            }
+        }
+        return dummy1;
+    
+    */
+
+    public static void main(String[] args) {
+        
+    }
+}
