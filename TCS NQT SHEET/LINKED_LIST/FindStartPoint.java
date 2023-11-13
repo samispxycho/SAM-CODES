b6acad6de28fc2972e9be1c443d7b1da3e5626b3
Sameer Upadhyay <itssamyhere17@gmail.com>
2023-11-13 21:10:02 +0530

    findstart

---
+++ TCS NQT SHEET/LINKED_LIST/FindStartPoint.java
@@ -0,0 +1,32 @@
+package LINKED_LIST;
+
+public class FindStartPoint {
+
+    /*
+    public ListNode detectCycle(ListNode head) {
+        if(head==null || head.next==null)return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode entry = head;
+        
+        while(fast.next!=null && fast.next.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                while(entry != slow){
+                    entry = entry.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;   
+    }
+    */
+
+    public static void main(String[] args) {
+        
+    }
+}
