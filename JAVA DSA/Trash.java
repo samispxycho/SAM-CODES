438db91bbe6031071b7b29e31040169908c22258
Sameer Upadhyay <itssamyhere17@gmail.com>
2024-02-19 22:50:26 +0530

    F

--- JAVA DSA/Trash.java
+++ JAVA DSA/Trash.java
@@ -1,24 +1,9 @@
-class A{
-    void display(){
-        System.out.println("A");
-    }
-}
-class B extends A{
-    void p(){
-        System.out.println("B");
-    }
-}
-
-class C extends B{
-    void ou(){
-        System.out.println("C");
-    }
-}
-
-
 public class Trash {
     public static void main(String[] args) {
-        C obj = new C();
-        obj.display();        
+        int[]arr={2,4,6,8,10};
+        int sq=0;
+        for(int i=0;i<2;i++){
+            sq=sq+arr[i]*arr[i];
+        }      
     }
 }
